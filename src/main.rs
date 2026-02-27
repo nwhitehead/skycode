9cbef581811fabf3e0338e1af7a35cc9e8cdd05f
Nathan Whitehead <nwhitehe@gmail.com>
2026-02-27 11:08:45 -0800

    Working minimal textarea demo.

--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,54 @@
-use std::{
-    io::{self, Read},
-    sync::{Arc, RwLock},
-    time::Duration,
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
+use ratatui::backend::CrosstermBackend;
+use ratatui::widgets::{Block, Borders};
+use ratatui::Terminal;
+use std::io;
+use tui_textarea::{Input, Key, TextArea};
+use ratatui::style::{Style, Color, Modifier};
 
-use crossterm::event::{self, Event, KeyCode, KeyEventKind};
-use portable_pty::{CommandBuilder, NativePtySystem, PtySize, PtySystem};
-use ratatui::{
-    DefaultTerminal, Frame,
-    layout::Alignment,
-    style::{Modifier, Style},
-    text::Line,
-    widgets::{Block, Borders, Paragraph},
-};
-use tui_term::widget::PseudoTerminal;
-use vt100::Screen;
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
 
-fn main() -> std::io::Result<()> {
-    let mut terminal = ratatui::init();
-    let result = run_app(&mut terminal);
-    ratatui::restore();
-    result
-}
+    enable_raw_mode()?;
+    crossterm::execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut term = Terminal::new(backend)?;
 
-fn run_app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    let pty_system = NativePtySystem::default();
-    let cwd = std::env::current_dir().unwrap();
-    let mut cmd = CommandBuilder::new("top");
-    cmd.cwd(cwd);
+    let mut textarea = TextArea::default();
+    textarea.set_max_histories(1000);
+    textarea.set_line_number_style(Style::default().fg(Color::DarkGray));
+    //textarea.set_cursor_line_style(Style::default().add_modifier(Modifier::ITALIC));
+    textarea.set_cursor_line_style(Style::default());
+    textarea.set_block(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Crossterm Minimal Example"),
+    );
 
-    let pair = pty_system
-        .openpty(PtySize {
-            rows: 24,
-            cols: 80,
-            pixel_width: 0,
-            pixel_height: 0,
-        })
-        .unwrap();
-    // Wait for the child to complete
-    std::thread::spawn(move || {
-        let mut child = pair.slave.spawn_command(cmd).unwrap();
-        let _child_exit_status = child.wait().unwrap();
-        drop(pair.slave);
-    });
-
-    let mut reader = pair.master.try_clone_reader().unwrap();
-    let parser = Arc::new(RwLock::new(vt100::Parser::new(24, 80, 0)));
-
-    {
-        let parser = parser.clone();
-        std::thread::spawn(move || {
-            // Consume the output from the child
-            // Can't read the full buffer, since that would wait for EOF
-            let mut buf = [0u8; 8192];
-            let mut processed_buf = Vec::new();
-            loop {
-                let size = reader.read(&mut buf).unwrap();
-                if size == 0 {
-                    break;
-                }
-                if size > 0 {
-                    processed_buf.extend_from_slice(&buf[..size]);
-                    let mut parser = parser.write().unwrap();
-                    parser.process(&processed_buf);
-
-                    // Clear the processed portion of the buffer
-                    processed_buf.clear();
-                }
-            }
-        });
-    }
-
-    {
-        // Drop writer on purpose
-        let _writer = pair.master.take_writer().unwrap();
-    }
-    drop(pair.master);
-
-    run(terminal, parser)
-}
-
-fn run(terminal: &mut DefaultTerminal, parser: Arc<RwLock<vt100::Parser>>) -> io::Result<()> {
     loop {
-        terminal.draw(|f| ui(f, parser.read().unwrap().screen()))?;
-
-        // Event read is blocking
-        if event::poll(Duration::from_millis(10))? {
-            // It's guaranteed that the `read()` won't block when the `poll()`
-            // function returns `true`
-            if let Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press {
-                    if let KeyCode::Char('q') = key.code {
-                        return Ok(());
-                    }
-                }
+        term.draw(|f| {
+            f.render_widget(&textarea, f.area());
+        })?;
+        match ratatui::crossterm::event::read()?.into() {
+            Input { key: Key::Esc, .. } => break,
+            input => {
+                textarea.input(input);
             }
         }
     }
-}
 
-fn ui(f: &mut Frame, screen: &Screen) {
-    let chunks = ratatui::layout::Layout::default()
-        .direction(ratatui::layout::Direction::Vertical)
-        .margin(1)
-        .constraints(
-            [
-                ratatui::layout::Constraint::Percentage(0),
-                ratatui::layout::Constraint::Percentage(100),
-                ratatui::layout::Constraint::Min(1),
-            ]
-            .as_ref(),
-        )
-        .split(f.area());
-    let title = Line::from("[ Running: top ]");
-    let block = Block::default()
-        .borders(Borders::ALL)
-        .title(title)
-        .style(Style::default().add_modifier(Modifier::BOLD));
-    let pseudo_term = PseudoTerminal::new(screen).block(block);
-    f.render_widget(pseudo_term, chunks[1]);
-    let block = Block::default().borders(Borders::ALL);
-    f.render_widget(block, f.area());
-    let explanation = "Press q to exit".to_string();
-    let explanation = Paragraph::new(explanation)
-        .style(Style::default().add_modifier(Modifier::BOLD | Modifier::REVERSED))
-        .alignment(Alignment::Center);
-    f.render_widget(explanation, chunks[2]);
+    disable_raw_mode()?;
+    crossterm::execute!(
+        term.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    term.show_cursor()?;
+
+    println!("Lines: {:?}", textarea.lines());
+    Ok(())
 }
